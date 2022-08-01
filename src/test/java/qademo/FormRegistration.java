a38696ac1564eacfd81f50a0c9257ae863d73f99
Maksim Shpak <shpak.point2010@yandex.ru>
2022-08-01 17:41:55 +0500

    rename class FormRegistration. Set property

--- src/test/java/qademo/FormRegistration.java
+++ src/test/java/qademo/FormRegistration.java
@@ -1,21 +1,22 @@
 package qademo;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FormRegistration {
     @BeforeAll
-    static void configure(){
+    public static void configuration() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
     }
+
     @Test
-    void fillFormTests(){
+    void fillFormTests() {
         open("/automation-practice-form");
         $("#firstName").setValue("Alexander");
         $("#lastName").setValue("Pushkin");
@@ -23,9 +24,11 @@ public class FormRegistration {
         $(".custom-control-label").click();
         $("#userNumber").setValue("9125986312");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").sendKeys("1799");
-        $(".react-datepicker__month-select").sendKeys("june");
-        $(".react-datepicker__day react-datepicker__day--006 react-datepicker__day--weekend").click();
+        $(".react-datepicker__year-select").selectOptionByValue("1997");
+        $(".react-datepicker__month-select").selectOptionByValue("5");
+        $(".react-datepicker__day--006").click();
+        $("#subjectsInput").setValue("History").pressEnter();
+        sleep(5000);
 
     }
 
