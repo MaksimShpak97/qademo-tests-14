c3e8eb6c9a68fc0f5e3c79dbb3bfcb25383f23a1
Maksim Shpak <shpak.point2010@yandex.ru>
2022-08-10 17:35:48 +0500

    refactoring page objects

--- src/test/java/qademo/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/qademo/tests/RegistrationFormWithPageObjectsTests.java
@@ -7,7 +7,6 @@ import qademo.pages.RegistrationFormPage;
 
 public class RegistrationFormWithPageObjectsTests {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-    String picture = "src\\test\\resources\\scrooge.png";
 
     @BeforeAll
     public static void configuration() {
@@ -18,7 +17,6 @@ public class RegistrationFormWithPageObjectsTests {
 
     @Test
     void fillFormTests() {
-
         registrationFormPage
                 .openPage()
                 .setFirstName("Alexander")
@@ -29,26 +27,14 @@ public class RegistrationFormWithPageObjectsTests {
                 .setBirthDate("06", "5", "1997")
                 .setSubject("History")
                 .setHobbies("2")
-                .addPicture(picture)
+                .addPicture("src\\test\\resources\\scrooge.png")
                 .setAddress("Pushkin street")
                 .selectState("Haryana")
                 .selectCity("Panipat")
                 .submit();
+
         registrationFormPage.checkResultTableVisible()
                 .cheсkResult("Student Name", "Alexander Pushkin");
-
-//        registrationFormPage.check("Student Name", "Alexander Pushkin");
-
-
-//        $("[type = 'submit']").click();
-
-//        $("div.modal-content").should(appear);
-//        $("div.modal-header").$(byText("Thanks for submitting the form")).shouldHave(text("Thanks for submitting the form"));
-//        SelenideElement modalBody = $(".modal-body");
-//        modalBody.shouldHave(text("Alexander Pushkin"), text("Pushkin@mail.ru"),
-//                text("Male"), text("9125986312"), text("06 June,1997"), text("History"),
-//                text("Sports"), text("scrooge.png"), text("Puchkin street"), text("Haryana Panipat"));
-//
 
     }
 
